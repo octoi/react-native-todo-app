54e1f70185aac834f142947b2d3e106ce52b0e83
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-06 11:53:46 +0530

    task

---
+++ components/Task.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function Task() {
+
+  return (
+    <View>
+      <Text>This is a task</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
